9f6bc08e806f558558f26e912ea1d0afacdcf9ea
Thaslima <nizuthasli15@gmail.com>
2023-10-05 16:56:07 +0530

    userroute

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,55 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../db/db');
-const { transport } = require('../utils/email');
-const generateOTP = require('../utils/generateOTP')
-// Create User Table if not exists
+const pool = require("../db/db");
+const { transport } = require("../utils/email");
+const generateOTP = require("../utils/generateOTP");
+const UserController = new (require("../Controller/UserController"))();
 
-// User Registration
-router.post('/register', (req, res) => {
-  const { email, password } = req.body;
-  // Check if email already exists
-  pool.query('SELECT * FROM users WHERE email = ? LIMIT 1', [email], (err, rows) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
-    if (rows.length > 0) {
-      // User with this email already exists
-      return res.status(200).json({ error: 'Email already exists' });
-    } else {
-      const otp = generateOTP();
-      pool.query('INSERT INTO users (email, password, verified, otp) VALUES (?, ?, ?, ?)', [email, password, false, otp], (err) => {
-        if (err) {
-          consol1e.error(err);
-          return res.status(500).json({ error: 'Internal server error' });
-        }
-        transport.sendMail(
-          {
-            from: process.env.EMAIL_FROM,
-            to: email,
-            subject: 'OTP Verification',
-            text: `Your OTP is: ${otp}`,
-          },
-          (err, info) => {
-            if (err) {
-              console.error(err);
-              return res.status(500).json({ message: 'Server error' });
-            }
-          }
-        );
-        const sql=`INSERT into userlogin (email,password) VALUES (?,?)`
-        pool.query(sql,[email, password],(err) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).json({ error: 'Internal server error' });
-        }
-        res.status(201).json({ message: 'User registered successfully' });
-    })
-        
-        
-      });
-    }
+router.post("/register", function (request, response) {
+  UserController.UserDetailsController(request, function ({ message, status }) {
+    console.log(status, message);
+    return response.status(status).send(message);
   });
 });
 module.exports = router;
--- frontend/src/Components/RegistrationForm.js
+++ frontend/src/Components/RegistrationForm.js
@@ -54,12 +54,18 @@ const RegistrationForm = () => {
           // } else {
           //   alert("Error sending OTP:", otpResponse.data);
           // }
+        } else if (registrationResponse.status == 301) {
+          window.alert("Email Already exists");
         } else {
           alert("Error registering user:", registrationResponse);
         }
       } catch (error) {
-        console.log(error);
-        alert("Error:", error);
+        if (error.response && error.response.status === 301) {
+          window.alert("Email already exists.");
+        } else {
+          console.error("Error creating user:", error);
+          window.alert("An error occurred");
+        }
       }
     },
   });
@@ -74,7 +80,6 @@ const RegistrationForm = () => {
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
-            
           }}
         >
           <AccountCircle style={{ fontSize: "64px", color: "#FBC91B" }} />{" "}
@@ -82,75 +87,87 @@ const RegistrationForm = () => {
           <h1 style={{ fontSize: "24px", color: "black" }}>Signup</h1>
           <img src={logoImage} alt="Your Logo" />
         </div>
-        <div >
-          <div style={{paddingLeft:"430px"}}>
-        <h4 style={{ alignItems: "center",paddingLeft:"140px" }}>PYRA-FIN</h4>
-        {!otpSent ? (
-          <form onSubmit={formik.handleSubmit}>
-            <TextField
-              name="email"
-              label="Email"
-              variant="outlined"
-              fullWidth
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.email}
-              style={{ marginBottom: "1rem", width: "100%", maxWidth: "350px" }}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-            />
-            <br />
-            <TextField
-              name="password"
-              label="Password"
-              type={showPassword ? "text" : "password"}
-              variant="outlined"
-              fullWidth
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.password}
-              style={{ marginBottom: "1rem", width: "100%", maxWidth: "350px" }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={handleTogglePassword}
-                      edge="end"
-                      aria-label="toggle password visibility"
-                    >
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-              error={formik.touched.password && Boolean(formik.errors.password)}
-              helperText={formik.touched.password && formik.errors.password}
-            />
-            <Grid container style={{ marginBottom: "1rem" }}>
-              <Grid item xs={12}>
-                <Link href="/register" variant="h2">
-                  {"Already have an account? Login"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              style={{ marginBottom: "1rem" }}
-            >
-              NEXT
-            </Button>
-          </form>
-        ) : (
-          <div>
-            <OTPValidationForm
-              email={formik.values.email}
-              password={formik.values.password}
-            />
+        <div>
+          <div style={{ paddingLeft: "430px" }}>
+            <h4 style={{ alignItems: "center", paddingLeft: "140px" }}>
+              PYRA-FIN
+            </h4>
+            {!otpSent ? (
+              <form onSubmit={formik.handleSubmit}>
+                <TextField
+                  name="email"
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  style={{
+                    marginBottom: "1rem",
+                    width: "100%",
+                    maxWidth: "350px",
+                  }}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  helperText={formik.touched.email && formik.errors.email}
+                />
+                <br />
+                <TextField
+                  name="password"
+                  label="Password"
+                  type={showPassword ? "text" : "password"}
+                  variant="outlined"
+                  fullWidth
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                  style={{
+                    marginBottom: "1rem",
+                    width: "100%",
+                    maxWidth: "350px",
+                  }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          onClick={handleTogglePassword}
+                          edge="end"
+                          aria-label="toggle password visibility"
+                        >
+                          {showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                  helperText={formik.touched.password && formik.errors.password}
+                />
+                <Grid container style={{ marginBottom: "1rem" }}>
+                  <Grid item xs={12}>
+                    <Link href="/register" variant="h2">
+                      {"Already have an account? Login"}
+                    </Link>
+                  </Grid>
+                </Grid>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  style={{ marginBottom: "1rem" }}
+                >
+                  NEXT
+                </Button>
+              </form>
+            ) : (
+              <div>
+                <OTPValidationForm
+                  email={formik.values.email}
+                  password={formik.values.password}
+                />
+              </div>
+            )}
           </div>
-        )}
-        </div>
         </div>
       </Container>
     </>
